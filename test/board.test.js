36b01306793123a6243459918e1a08dde7172ee9
Heather <80366350+heathjackson@users.noreply.github.com>
2023-11-07 14:21:48 -0500

    fixed broken test

--- test/board.test.js
+++ test/board.test.js
@@ -1,17 +1,5 @@
 import { Board } from "../src/js/model/board";
 
-describe('#isEmpyt', () => {
-  test('returns true if linked list is empty', () => {
-    const ll = Board.fromValues()
-    expect(ll.isEmpty()).toBe(true)
-  })
-  
-  test('returns false if linked list is not empty', () => {
-    const ll = Board.fromValues(10, 20)
-    expect(ll.isEmpty()).toBe(false)
-  })
-})
-
 describe('#addSpace', () => {
   const ll = Board.fromValues(20, 30, 40)
   test('add a space to a linked list at the beginning of the list', () => {
